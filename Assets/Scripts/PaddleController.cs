0cf0048dab3e0438b8dd5acfe767ad851f28d2e5
Ahmadboygg <97325312+Ahmadboygg@users.noreply.github.com>
2023-04-06 06:28:42 +0700

    Base gameplay

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour
 {
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public int speed = 0;
     void Start()
     {
         
@@ -11,6 +14,26 @@ public class PaddleController : MonoBehaviour
 
     void Update()
     {
-        
+        MoveObject(GetInput());
+    }
+
+    Vector2 GetInput()
+    {
+
+        if(Input.GetKey(upKey))
+        {
+           return Vector2.up;
+        }
+        else if(Input.GetKey(downKey))
+        {
+           return Vector2.down;
+        }
+
+        return Vector2.zero;
+    }
+
+    void MoveObject(Vector2 movement)
+    {
+        transform.Translate(movement * speed * Time.deltaTime);
     }
 }
