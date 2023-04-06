9f332804402f9cfc09984ba740b936c3ba516d9b
Ahmadboygg <97325312+Ahmadboygg@users.noreply.github.com>
2023-04-06 21:24:10 +0700

    Base gameplay

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -7,9 +7,11 @@ public class PaddleController : MonoBehaviour
     public KeyCode upKey;
     public KeyCode downKey;
     public int speed = 0;
+
+    private Rigidbody2D rigidbody2D;
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
@@ -22,11 +24,11 @@ public class PaddleController : MonoBehaviour
 
         if(Input.GetKey(upKey))
         {
-           return Vector2.up;
+           return Vector2.up * speed;
         }
         else if(Input.GetKey(downKey))
         {
-           return Vector2.down;
+           return Vector2.down * speed;
         }
 
         return Vector2.zero;
@@ -34,6 +36,6 @@ public class PaddleController : MonoBehaviour
 
     void MoveObject(Vector2 movement)
     {
-        transform.Translate(movement * speed * Time.deltaTime);
+        rigidbody2D.velocity = movement;
     }
 }
